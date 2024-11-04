976a5375fa8d1b9e99c474eef847edab49426e40
Rachel-Braun <r0527174254@gmail.com>
2024-11-04 15:37:55 +0200

    Add project files.

---
+++ TheGame/zzzzzzzzzzzzzzzzzzzzzzzzzzzzz.cpp
@@ -0,0 +1,17 @@
+
+/*
++World
+3 1 4 5 6 3 2 1 4 3 2 5 6
+1 1 1 3 6 3 2 1 4 4 5 4 3
+5 1 6 3 2 5 4 3 4 1 2 3 1
+3 1 4 5 6 3 2 1 4 3 2 5 6
+1 1 1 3 6 3 2 1 4 4 5 4 3
+5 1 6 3 2 5 4 3 4 1 2 3 1
++Start
+Resource 1 Wood 1 1
++Input
+Select 1 1
++Asserts
+SelectedResource
+*/
+
